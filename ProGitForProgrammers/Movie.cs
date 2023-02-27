9507bbbfa9232adc36cee13fd3008160edead93b
Francisco Javier Montecillo Puente <frajavimopu@gmail.com>
2023-02-27 16:57:51 -0600

    Update Movie.cs
    Add Tittle and Actors

--- ProGitForProgrammers/Movie.cs
+++ ProGitForProgrammers/Movie.cs
@@ -1,3 +1,7 @@
-﻿public class Movie
-{
+﻿namespace ProGitForProgrammers {
+    public class Movie
+    {
+        public string Title { get; set; }
+        pubblic List<string> Actors { get; set; }
+    }
 }
