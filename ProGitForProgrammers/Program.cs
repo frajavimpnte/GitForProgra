a484efe18f28459f19a3acdd944b2970d71ec77c
Francisco Javier Montecillo Puente <frajavimopu@gmail.com>
2023-02-27 15:30:31 -0600

    Demostrating the stagin area

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I just added this in Visual Studio");
             Console.WriteLine("I just added this in the command line repo");
-        }
+            Console.WriteLine("This line adde to show the stagin area");
+            }
     }
 }
