a9d29f89ea9a597e74e95adcca4de5e0b8d932be
Francisco Javier Montecillo Puente <frajavimopu@gmail.com>
2023-02-27 15:14:43 -0600

    Add writeline indicating we are in command line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
         }
     }
 }
